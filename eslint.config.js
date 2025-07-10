0941eaf0d20af299579f23cab92b3a02fddb5bc6
sokheang <sokheang.dev@gmail.com>
2025-07-10 20:30:17 +0700

    Add linting

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+// @ts-check
+import eslint from "@eslint/js";
+import { config, configs as _configs } from "typescript-eslint";
+import { configs as __configs, processInlineTemplates } from "angular-eslint";
+
+const { configs } = eslint;
+
+export default config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      configs.recommended,
+      ..._configs.recommended,
+      ..._configs.stylistic,
+      ...__configs.tsRecommended,
+    ],
+    processor: processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...__configs.templateRecommended,
+      ...__configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+);
