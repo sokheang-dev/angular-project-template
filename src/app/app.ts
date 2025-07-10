e0b4b60e3105fc20622fbbe0e0907316eff6662b
sokheang <sokheang.dev@gmail.com>
2025-07-10 20:38:12 +0700

    Renaming project

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('my-app');
+  protected readonly title = signal('angular-project-template');
 }
