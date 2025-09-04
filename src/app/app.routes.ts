3cc715fce7e6aea5a740691040633f7938b1fdfd
sokheang.dev <sokheang.dev@gmail.com>
2025-09-05 03:00:06 +0700

    Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
-import { Routes } from '@angular/router';
-import { authGuard } from './core/guards/auth-guard';
+import { Routes } from "@angular/router";
+import { authenticatedGuard } from "./core/guards/authenticated-guard";
+import { unauthenticatedGuard } from "./core/guards/unauthenticated-guard";
 
 export const routes: Routes = [
-    {
-        path: 'login',
-        loadComponent: () => import("./features/login/login").then(m => m.Login),
-        canActivate: [authGuard]
-    },
+  {
+    path: "",
+    loadComponent: () => import("./features/home/home").then((m) => m.Home),
+    canActivate: [authenticatedGuard],
+  },
+  {
+    path: "login",
+    loadComponent: () => import("./features/login/login").then((m) => m.Login),
+    canActivate: [unauthenticatedGuard],
+  },
 ];
