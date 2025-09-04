b57079fed806ba3356dc25a71c9b5054e28baf96
sokheang.dev <sokheang.dev@gmail.com>
2025-09-05 02:56:41 +0700

    Implement auth store

--- src/app/core/stores/auth-store.ts
+++ src/app/core/stores/auth-store.ts
@@ -1,11 +1,32 @@
-import { inject, Injectable } from "@angular/core";
+import { inject, Injectable, signal } from "@angular/core";
 import { SsrCookieService } from "ngx-cookie-service-ssr";
+import { jwtDecode } from "jwt-decode";
 
+const JWT = "_jwt";
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthStore {
-    private readonly cookie = inject(SsrCookieService);
+  private readonly cookie = inject(SsrCookieService);
+  private readonly token = signal(this.cookie.get(JWT));
+  private readonly claims = signal<string[]>([]);
 
+  authenticated(): boolean {
+    return this.token() !== "";
+  }
 
+  setToken(token: string) {
+    this.cookie.set(JWT, token);
+    this.token.set(token);
+    this.decode(token);
+  }
+
+  getUsername() {}
+
+  private decode(token: string) {
+    const payload = jwtDecode(token);
+    console.log(payload);
+    const claims: string[] = [];
+    this.claims.set(claims);
+  }
 }
